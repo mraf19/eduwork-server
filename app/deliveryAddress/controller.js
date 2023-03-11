9d92512d9610d9d65518edcde60a596cc0415355
mraf19 <raflimre1902@gmail.com>
2023-03-12 04:25:18 +0700

    edit

--- app/deliveryAddress/controller.js
+++ app/deliveryAddress/controller.js
@@ -3,120 +3,121 @@ const { policyfor } = require("../../utils");
 const delivAdd = require("./model");
 
 const store = async (req, res, next) => {
-	try {
-		const payload = req.body;
-		const user = req.user;
-		const delivery = new delivAdd({ ...payload, user: user._id });
-		await delivery.save();
-		res.json({
-			message: "address uploaded successfully",
-			deliveryAddress: delivery,
-		});
-	} catch (err) {
-		if (err && err.name === "ValidationError") {
-			return res.json({
-				error: 1,
-				message: err.message,
-				fields: err.errors,
-			});
-		}
+  try {
+    const payload = req.body;
+    console.log(payload);
+    const user = req.user;
+    const delivery = new delivAdd({ ...payload, user: user._id });
+    await delivery.save();
+    res.json({
+      message: "address uploaded successfully",
+      deliveryAddress: delivery,
+    });
+  } catch (err) {
+    if (err && err.name === "ValidationError") {
+      return res.json({
+        error: 1,
+        message: err.message,
+        fields: err.errors,
+      });
+    }
 
-		next(err);
-	}
+    next(err);
+  }
 };
 
 const index = async (req, res, next) => {
-	try {
-		let { skip = 0, limit = 10 } = req.query;
-		let count = await delivAdd.find({ user: req.user._id }).countDocuments();
-		const address = await delivAdd
-			.find({ user: req.user._id })
-			.skip(parseInt(skip))
-			.limit(parseInt(limit))
-			.sort("-createdAt");
-		res.json(address);
-	} catch (err) {
-		if (err && err.name === "ValidationError") {
-			return res.json({
-				error: 1,
-				message: err.message,
-				fields: err.errors,
-			});
-		}
+  try {
+    let { skip = 0, limit = 10 } = req.query;
+    let count = await delivAdd.find({ user: req.user._id }).countDocuments();
+    const address = await delivAdd
+      .find({ user: req.user._id })
+      .skip(parseInt(skip))
+      .limit(parseInt(limit))
+      .sort("-createdAt");
+    res.json(address);
+  } catch (err) {
+    if (err && err.name === "ValidationError") {
+      return res.json({
+        error: 1,
+        message: err.message,
+        fields: err.errors,
+      });
+    }
 
-		next(err);
-	}
+    next(err);
+  }
 };
 
 const update = async (req, res, next) => {
-	try {
-		let { _id, ...payload } = req.body;
-		let { id } = req.params;
-		let address = await delivAdd.findById(id);
-		let subjectAddress = subject("DeliveryAdress", {
-			...address,
-			user_id: address.user,
-		});
-		let policy = policyfor(req.user);
-		if (!policy.can("update", subjectAddress)) {
-			return res.json({
-				error: 1,
-				message: "You are not allowed to modify this resource",
-			});
-		}
-		address = await delivAdd.findByIdAndUpdate(id, payload, { new: true });
-		res.json({
-			message: "Address updated successfully",
-			deliveryAddress: address,
-		});
-	} catch (err) {
-		if (err && err.name === "ValidationError") {
-			return res.json({
-				error: 1,
-				message: err.message,
-				fields: err.errors,
-			});
-		}
+  try {
+    let { _id, ...payload } = req.body;
+    let { id } = req.params;
+    let address = await delivAdd.findById(id);
+    let subjectAddress = subject("DeliveryAdress", {
+      ...address,
+      user_id: address.user,
+    });
+    let policy = policyfor(req.user);
+    if (!policy.can("update", subjectAddress)) {
+      return res.json({
+        error: 1,
+        message: "You are not allowed to modify this resource",
+      });
+    }
+    address = await delivAdd.findByIdAndUpdate(id, payload, { new: true });
+    res.json({
+      message: "Address updated successfully",
+      deliveryAddress: address,
+    });
+  } catch (err) {
+    if (err && err.name === "ValidationError") {
+      return res.json({
+        error: 1,
+        message: err.message,
+        fields: err.errors,
+      });
+    }
 
-		next(err);
-	}
+    next(err);
+  }
 };
 
 const destroy = async (req, res, next) => {
-	try {
-		let { id } = req.params;
-		let address = await delivAdd.findById(id);
-		let subjectAddress = subject("DeliveryAddress", {
-			...address,
-			user_id: address.user,
-		});
-		let policy = policyfor(req.user);
-		if (!policy.can("delete", subjectAddress)) {
-			return res.json({
-				error: 1,
-				message: "You are not allowed to delete this resource",
-			});
-		}
-		address = await delivAdd.findByIdAndDelete(id);
-		res.json({
-			message: "Address deleted successfully",
-			data: address,
-		});
-	} catch (err) {
-		if (err && err.name === "ValidationError") {
-			return res.json({
-				error: 1,
-				message: err.message,
-				fields: err.errors,
-			});
-		}
+  try {
+    let { id } = req.params;
+    let address = await delivAdd.findById(id);
+    let subjectAddress = subject("DeliveryAddress", {
+      ...address,
+      user_id: address.user,
+    });
+    let policy = policyfor(req.user);
+    if (!policy.can("delete", subjectAddress)) {
+      return res.json({
+        error: 1,
+        message: "You are not allowed to delete this resource",
+      });
+    }
+    address = await delivAdd.findByIdAndDelete(id);
+    res.json({
+      message: "Address deleted successfully",
+      data: address,
+    });
+  } catch (err) {
+    if (err && err.name === "ValidationError") {
+      return res.json({
+        error: 1,
+        message: err.message,
+        fields: err.errors,
+      });
+    }
 
-		next(err);
-	}
+    next(err);
+  }
 };
 module.exports = {
-	store,
-	index,
-	update,
-	destroy,
+  store,
+  index,
+  update,
+  destroy,
 };
--- app/tag/controller.js
+++ app/tag/controller.js
@@ -3,111 +3,115 @@ const Category = require("../category/model");
 const Product = require("../product/model");
 
 const index = async (req, res, next) => {
-	try {
-		let tags = await Tag.find();
-		return res.json(tags);
-	} catch (err) {
-		if (err && err.name === "ValidationError") {
-			return res.json({
-				error: 1,
-				message: err.message,
-				fields: err.errors,
-			});
-		}
+  try {
+    let tags = await Tag.find();
+    return res.json(tags);
+  } catch (err) {
+    if (err && err.name === "ValidationError") {
+      return res.json({
+        error: 1,
+        message: err.message,
+        fields: err.errors,
+      });
+    }
 
-		next(err);
-	}
+    next(err);
+  }
 };
 
 const store = async (req, res, next) => {
-	try {
-		let payload = req.body;
-		let tag = await new Tag(payload);
-		await tag.save();
-		return res.json(tag);
-	} catch (err) {
-		if (err && err.name === "ValidationError") {
-			return res.json({
-				error: 1,
-				message: err.message,
-				fields: err.errors,
-			});
-		}
+  try {
+    let payload = req.body;
+    let tag = await new Tag(payload);
+    await tag.save();
+    return res.json(tag);
+  } catch (err) {
+    if (err && err.name === "ValidationError") {
+      return res.json({
+        error: 1,
+        message: err.message,
+        fields: err.errors,
+      });
+    }
 
-		next(err);
-	}
+    next(err);
+  }
 };
 
 const update = async (req, res, next) => {
-	try {
-		let payload = req.body;
-		let tag = await Tag.findByIdAndUpdate(req.params.id, payload, {
-			new: true,
-			runValidators: true,
-		});
-		return res.json(tag);
-	} catch (err) {
-		if (err && err.name === "ValidationError") {
-			return res.json({
-				error: 1,
-				message: err.message,
-				fields: err.errors,
-			});
-		}
+  try {
+    let payload = req.body;
+    let tag = await Tag.findByIdAndUpdate(req.params.id, payload, {
+      new: true,
+      runValidators: true,
+    });
+    return res.json(tag);
+  } catch (err) {
+    if (err && err.name === "ValidationError") {
+      return res.json({
+        error: 1,
+        message: err.message,
+        fields: err.errors,
+      });
+    }
 
-		next(err);
-	}
+    next(err);
+  }
 };
 
 const destroy = async (req, res, next) => {
-	try {
-		let tag = await Tag.findByIdAndDelete({ _id: req.params.id });
-		return res.json(tag);
-	} catch (err) {
-		if (err && err.name === "ValidationError") {
-			return res.json({
-				error: 1,
-				message: err.message,
-				fields: err.errors,
-			});
-		}
+  try {
+    let tag = await Tag.findByIdAndDelete({ _id: req.params.id });
+    return res.json(tag);
+  } catch (err) {
+    if (err && err.name === "ValidationError") {
+      return res.json({
+        error: 1,
+        message: err.message,
+        fields: err.errors,
+      });
+    }
 
-		next(err);
-	}
+    next(err);
+  }
 };
 
 const showTagByCategory = async (req, res, next) => {
-	try {
-		const { category } = req.params;
-		const categoryId = await Category.findOne({
-			name: { $regex: category, $options: "i" },
-		});
-		console.log(categoryId)
-		const products = await Product.find({ category: categoryId });
-		let tagIds = [];
-		products.forEach((product) => {			
-			tagIds.push(product.tags);		
-		});
+  try {
+    const { category } = req.params;
+    const category_id = await Category.findOne({
+      name: { $regex: category, $options: "i" },
+    });
+    const products = await Product.find({ category: category_id });
+    let tagIds = [];
+    products.forEach((product) => {
+      console.log(product.tags);
+      product.tags.forEach((tag) => {
+        if (!tagIds.includes(tag)) {
+          tagIds.push(tag);
+        }
+      });
+    });
 
-		const tags = await Tag.find({ _id: { $in: tagIds } });
-		res.json(tags);
-	} catch (err) {
-		if (err && err.name === "ValidationError") {
-			return res.json({
-				error: 1,
-				message: err.message,
-				fields: err.errors,
-			});
-		}
+    const tags = await Tag.find({ _id: { $in: tagIds } });
+    res.json(tags);
+  } catch (err) {
+    if (err && err.name === "ValidationError") {
+      return res.json({
+        error: 1,
+        message: err.message,
+        fields: err.errors,
+      });
+    }
 
-		next(err);
-	}
+    next(err);
+  }
 };
 
 module.exports = {
-	index,
-	store,
-	update,
-	destroy,
-	showTagByCategory,
+  index,
+  store,
+  update,
+  destroy,
+  showTagByCategory,
 };
